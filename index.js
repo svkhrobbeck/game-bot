4f51c9dd391bdc94fb6a2c4f952dd622d92b9038
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-25 23:35:09 +0500

    Create options.js for keyboard options

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
+const { gameOptions, restartOptions, startOptions } = require("./options");
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 let randomNum;
-
-// options
-const startOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "O'yin o'ynash", callback_data: "/game" }]],
-  }),
-};
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: 1, callback_data: 1 },
-        { text: 2, callback_data: 2 },
-        { text: 3, callback_data: 3 },
-      ],
-      [
-        { text: 4, callback_data: 4 },
-        { text: 5, callback_data: 5 },
-        { text: 6, callback_data: 6 },
-      ],
-      [
-        { text: 7, callback_data: 7 },
-        { text: 8, callback_data: 8 },
-        { text: 9, callback_data: 9 },
-      ],
-      [{ text: 0, callback_data: 0 }],
-    ],
-  }),
-};
-
-const restartOptions = {
-  reply_markup: JSON.stringify({ inline_keyboard: [[{ text: "Qayta o'ynash", callback_data: "/again" }]] }),
-};
 
 // functions
 const startGame = async chatId => {
